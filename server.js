95fcbe012d2b2069d8d2a7b4b7e8c4b4e445be63
Tyler <realpsych22@gmail.com>
2020-04-09 12:16:26 -0500

    set up files per readme

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var runner            = require('./test-runner');
 
 var app = express();
 
+app.use(helmet.noCache());
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
