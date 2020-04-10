5cf77f3c4a65afa1e2776197ad1577b2c965e76b
Tyler <realpsych22@gmail.com>
2020-04-09 19:50:53 -0500

    m

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,10 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const MONGODB_CONNECTION_STRING = process.env.DB;
+mongoose.connect(MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+  if(err){console.log("error")}
+});
 
 var Schema = mongoose.Schema;
 
@@ -34,7 +37,7 @@ module.exports = function (app) {
       if (!data){res.send( 'no book exists')}
       if(err) res.send("no data found that matches params");
       else  {
-        data.commentCount = data.comments
+        console.log(data.comments)
         res.send(data);
       }
      })
@@ -46,24 +49,24 @@ module.exports = function (app) {
     .post(function (req, res){
     if(!req.body.title){res.send("Please include a title")}
     else{
-      var title = req.body.title;
       var newBook=new Book({
-        title: title
+        title: req.body.title
       })
-    newBook.save;
+    newBook.save();
     res.send(newBook);
     }
     })
     
-    .delete(function(req, res){
+ //   .delete(function(req, res){
       //if successful response will be 'complete delete successful'
-    });
+ //   });
 
 
 
   app.route('/api/books/:id')
     .get(function (req, res){
-      var bookid = req.params.id;
+      var bookid = req.query.id;
+    console.log(req.params)
      var collection = Book.findById(bookid, function(err, data){ 
       if (!data){
         res.send( 'no book exists')}
